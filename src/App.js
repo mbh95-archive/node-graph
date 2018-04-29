b19790facdac302196184fad0554d84b9e328671
mbh95 <matthewbh95@gmail.com>
2018-04-28 18:28:23 -0700

    Drew to a canvas! And added more Functions.:

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Abs, Plus} from './Functions.js'
+import {Const, Abs, Plus, Mult, Cos, RectToPolar} from './Functions.js'
 
 class App extends Component {
   constructor() {
@@ -11,54 +11,91 @@ class App extends Component {
       h: 100,
       time: 0.0,
       finalOut: {
-        r: "absOut",
-        g: "absOut",
-        b: "absOut"
+        r: "grayOut",
+        g: "grayOut",
+        b: "grayOut"
       },
       funcs: [
-        new Plus("x", "y", "xpy"),
-        new Abs("xpy", "absOut")
+        new Cos("scaledR", "grayOut"),
+        new Const(6.28, "2pi"),
+        new Mult("r", "2pi", "scaledR"),
+        new RectToPolar("x", "y", "r", "theta")
       ],
       funcFromOutId: {},
     };
+    this.canvas = React.createRef();
 
     // Generate the dependency map
     // Map output ids to list of dependency input ids
-    var funcFromOutId = {}
+    let funcFromOutId = {}
     const funcs = this.state.funcs;
-    for (var i = 0; i < funcs.length; i++) {
+    for (let i = 0; i < funcs.length; i++) {
       const func = funcs[i];
-      for (var outName in func.outputIds) {
+      for (let outName in func.outputIds) {
         const outId = func.outputIds[outName];
         funcFromOutId[outId] = func;
       }
     }
     this.state.funcFromOutId = funcFromOutId;
-    console.log(funcFromOutId);
+    // console.log(funcFromOutId);
     this.runGraph(-2,-8,3);
+  }
+
+  screenToWorldX(x) {
+    const screenWidth = this.canvas.current.width;
+    return 2 * (x / screenWidth) - 1;
+  }
+
+  screenToWorldY(y) {
+    const screenHeight = this.canvas.current.height;
+    return 2 * (y / screenHeight) - 1;
+  }
+
+  colorRangeToByte(channel) {
+    return Math.floor(channel*255);
+  }
+
+  draw(t) {
+    const canvas = this.canvas.current;
+    const imageData = this.ctx.createImageData(canvas.width, canvas.height);
+
+    for (let y = 0; y < canvas.height; y++) {
+      const worldY = this.screenToWorldY(y);
+      const yByteOffset = 4*y * canvas.width;
+      for (let x = 0; x < canvas.height; x++) {
+        const worldX = this.screenToWorldX(x);
+        let {r, g, b} = this.runGraph(worldX, worldY, t);
+        const byteOffset = yByteOffset + (x * 4);
+        imageData.data[byteOffset + 0] = this.colorRangeToByte(r);
+        imageData.data[byteOffset + 1] = this.colorRangeToByte(g);
+        imageData.data[byteOffset + 2] = this.colorRangeToByte(b);
+        imageData.data[byteOffset + 3] = 255;
+      }
+    }
+    this.ctx.putImageData(imageData, 0, 0);
   }
 
   runGraph(x, y, t) {
     const funcs = this.state.funcs;
-    var stack = [];
-    var cache = {
+    let stack = [];
+    let cache = {
       x: x,
       y: y,
       t: t
     };
 
     //Generate the initial stack
-    for (var finalOutName in this.state.finalOut) {
+    for (let finalOutName in this.state.finalOut) {
       const finalOutValueId = this.state.finalOut[finalOutName];
       stack.push(finalOutValueId);
     }
-    console.log(stack);
+    // console.log(stack);
 
     while(stack.length > 0) {
       const topId = stack[stack.length - 1];
-      console.log(topId);
-      console.log(stack);
-      console.log(cache);
+      // console.log(topId);
+      // console.log(stack);
+      // console.log(cache);
 
       if (topId in cache) {
         stack.pop();
@@ -66,12 +103,12 @@ class App extends Component {
         const topFunc = this.state.funcFromOutId[topId];
         const unknownDeps = Object.values(topFunc.inputIds).filter(dep => !(dep in cache));
         if (unknownDeps.length == 0) {
-          var inputs = {};
-          for (var inputName in topFunc.inputIds) {
+          let inputs = {};
+          for (let inputName in topFunc.inputIds) {
             inputs[inputName] = cache[topFunc.inputIds[inputName]];
           }
           const outputs = topFunc.eval(inputs);
-          for (var outputName in topFunc.outputIds) {
+          for (let outputName in topFunc.outputIds) {
             cache[topFunc.outputIds[outputName]] = outputs[outputName];
           }
           stack.pop();
@@ -80,19 +117,20 @@ class App extends Component {
         }
       }
     }
+    const finalOut = this.state.finalOut;
+    return {r: cache[finalOut.r], g: cache[finalOut.g], b: cache[finalOut.b]};
   }
 
   componentDidMount() {
-    const canvas = this.refs.canvas;
-    const ctx = canvas.getContext("2d");
-    ctx.moveTo(0, 0);
-    ctx.lineTo(200, 100);
-    ctx.stroke();
+    const canvas = this.canvas.current;
+    this.ctx = canvas.getContext("2d");
+    this.draw(0);
   }
+
   render() {
     return (
       <div className="App">
-        <canvas ref="canvas" width={200} height={100} />
+        <canvas ref={this.canvas} width={100} height={100} />
       </div>
     );
   }
--- src/Functions.js
+++ src/Functions.js
@@ -8,6 +8,19 @@ class Function {
   }
 }
 
+export class Const extends Function {
+  constructor(val, outId) {
+    super();
+    this.type = "Const";
+    this.outputIds.out = outId;
+    this.val = val;
+  }
+
+  eval(inputs) {
+    return {out:this.val};
+  }
+}
+
 export class Abs extends Function {
   constructor(opId, absId) {
     super();
@@ -32,5 +45,52 @@ export class Plus extends Function {
 
   eval(inputs) {
     return {sum: (inputs.op1 + inputs.op2)};
+  }
+}
+
+export class Mult extends Function {
+  constructor(op1Id, op2Id, prodId) {
+    super();
+    this.type = "Mult";
+    this.inputIds.op1 = op1Id;
+    this.inputIds.op2 = op2Id;
+    this.outputIds.prod = prodId;
+  }
+
+  eval(inputs) {
+    return {prod: (inputs.op1 * inputs.op2)};
+  }
+}
+
+export class RectToPolar extends Function {
+  constructor(xId, yId, rId, thetaId) {
+    super();
+    this.type = "RectToPolar";
+    this.inputIds.x = xId;
+    this.inputIds.y = yId;
+    this.outputIds.r = rId;
+    this.outputIds.theta = thetaId;
+  }
+
+  eval(inputs) {
+    return {
+      r: inputs.x ** 2 + inputs.y ** 2,
+      theta: Math.atan2(inputs.y, inputs.x)
+    };
+  }
+}
+
+export class Cos extends Function {
+  constructor(thetaId, outId) {
+    super();
+    this.type = "Cos";
+    this.inputIds.theta = thetaId;
+    this.outputIds.out = outId;
+  }
+
+  eval(inputs) {
+    return {
+      out: Math.cos(inputs.theta)
+    };
   }
 }
